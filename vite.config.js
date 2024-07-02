aafcb5c337402e19eb4d7633221a857bcc0ae23d
chang180 <chang180@gmail.com>
2024-07-02 17:28:58 +0800

    Prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    watch: {
-      usePolling: true, // 針對文件變更的輪詢設置
-      interval: 1000, // 每秒檢查文件變更一次
-    },
-  },
-})
+  base: '/weather-map/', // 替換為您的 repository 名稱
+});
