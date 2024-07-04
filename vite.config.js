66782651656218279f6f1aecb9063358ac487c88
CHANG180-PC\chang <chang180@gmail.com>
2024-07-05 00:04:39 +0800

    back to  normal

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   base: '/weather-map/', // 替換為您的 repository 名稱
+  build: {
+    manifest: true,
+    outDir: 'dist', // 替換為您的 repository 名稱
+  },
 });
