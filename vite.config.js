740938dca2f24151f24b782ae6d60f2eb2fc4a8d
chang180 <chang180@gmail.com>
2024-07-04 23:05:56 +0800

    Save local changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/weather-map/', // 替換為您的 repository 名稱
+  base: '/weather-map/',
+  build: {
+    outDir: 'dist',
+    manifest: true,
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      },
+    },
+  },
+  server: {
+    port: 3000,
+  },
 });
